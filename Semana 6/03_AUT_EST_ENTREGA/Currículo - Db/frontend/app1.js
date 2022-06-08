1442d05a084437025919136ed3302936b6fe61a3
Dayllan de Souza Alho <dayllan.alho@sou.inteli.edu.br>
2022-06-08 17:58:31 -0300

    :sweat_smile: Update dos Currículos ajustados e funcionando
    Desde que inclui o banco de dados na semana 6 o  botão que direciona para o LinkedIn não funcionou mais.
    Este erro foi observado e corrigido a partir de hoje (08/06/2022)

--- Semana 6/03_AUT_EST_ENTREGA/Currículo - Db/frontend/app1.js
+++ Semana 6/03_AUT_EST_ENTREGA/Currículo - Db/frontend/app1.js
@@ -1,5 +1,3 @@
-const { response } = require("express")
-
 const button = document.querySelector('button')
 const popup = document.querySelector('.popup-wrapper')
 
@@ -18,32 +16,4 @@ function tempo (req,res){
     })
 }
 
-
-
-
-/*
-const curriculo = {
-    experiencias:[
-        {
-            empresas_contratantes: [Inteli, Colégio_Gomes_Pereira,
-            Curso_PeC],
-            cargos_obtidos: [estagiario, professor, desenvolvedor],
-        }
-    ],
-    tempo_de_trabalho:[
-        {
-            tempo: [até_1_ano, entre_1_a_3_anos, mais_que_3_anos]
-        }
-    ]
-};
-
-function nova_experiencia ({owner, content}){
-    curriculo.experiencias.push({
-        owner: dados.owner,
-        content: dados.content
-    });
-    criar_experiencia({owner:'Inteli', content: 'estagiario'})
-    
-    }
-    console.log(curriculo)
-    */
+const { response } = require("express")
